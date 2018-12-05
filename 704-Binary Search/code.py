2baeed5b94427ead50d6e6d8eaff1202a02b5bb2
remember <42919833+remembed@users.noreply.github.com>
2018-12-05 22:55:54 +0800

    Create code.py

---
+++ 704-Binary Search/code.py
@@ -0,0 +1,25 @@
+#one way:
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+
+        i=0
+        j=len(nums)-1
+        while i<=j:
+            mid = (i+j)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                j = mid -1
+            else:
+                i = mid+1
+        return -1
+        #tow way:
+        if target in nums:
+            return nums.index(target)
+        else:return -1
